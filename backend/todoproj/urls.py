d3cf79a7d2c4ced7321857dab26791569fc7e289
Jose Pinto <pintojose.benedicto2@gmail.com>
2019-10-30 04:33:02 -0600

    Added handling for creating, updating and deleting tasks

--- backend/todoproj/urls.py
+++ backend/todoproj/urls.py
@@ -20,4 +20,5 @@ from api import views as api_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/task/', api_views.TaskList.as_view(), name='task-list'),
+    path('api/task/<int:task_id>/', api_views.TaskDetail.as_view(), name='task-detail'),
 ]
